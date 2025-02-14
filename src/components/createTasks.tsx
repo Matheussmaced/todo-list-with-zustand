883863e6e9cb98698d86bd7e6304a0f3adef3ae5
Matheussmaced <mateusbyakuya3@gmail.com>
2025-02-14 19:44:13 -0300

    feat: capturing data from my input

--- src/components/createTasks.tsx
+++ src/components/createTasks.tsx
@@ -1,11 +1,24 @@
+'use client'
+
+import { FormEvent, useState } from "react";
+
 export default function CreateTasks() {
+  const [newTask, setNewTask] = useState('');
+
+  const handleAddTask = (e: FormEvent) => {
+    e.preventDefault();
+    setNewTask('');
+  }
+
   return (
     <main className="w-[432]">
-      <form action="" className="flex flex-row gap-3">
-        <input type="text" placeholder="Adicione uma nova tarefa" className="bg-bg border border-borderInput ... flex-1 py-2 px-4 text-gray-400 rounded-md focus:outline-none" />
+      <form onSubmit={handleAddTask} className="flex flex-row gap-3">
+
+        <input type="text" value={newTask} placeholder="Adicione uma nova tarefa" onChange={(e) => setNewTask(e.target.value)} className="bg-bg border border-borderInput ... flex-1 py-2 px-4 text-gray-400 rounded-md focus:outline-none" />
         <button type="submit" className=" bg-todoText px-3 py-0.5 rounded-md text-2xl text-center">
           +
         </button>
+
       </form>
     </main>
   );
